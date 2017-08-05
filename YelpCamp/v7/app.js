45466aa5409972005c403adfe04b0dcff792bffb
Miao Yang <yangmiao168@gmail.com>
2017-08-05 19:27:26 +0000

    added files

---
+++ YelpCamp/v7/app.js
@@ -0,0 +1,61 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy  = require("passport-local"),
+    Campground = require("./models/campground"),
+    Comment    = require("./models/comment"),
+    User = require("./models/user"),
+    seedDB   = require ("./seeds")
+    
+  var commentRoutes = require("./routes/comments"),
+      campgroundRoutes = require("./routes/campgrounds"),
+      indexRoutes      = require("./routes/index");
+    
+seedDB();
+mongoose.connect("mongodb://localhost/yelp_camp_v7");
+app.use(bodyParser.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+
+var campgrounds = [
+        
+        {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"},
+        {name: "Desert Mesa",  image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg"},
+        {name: "Canyon Floor", image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg"},
+         {name: "Salmon Creek", image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg"},
+        {name: "Desert Mesa",  image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg"},
+        {name: "Canyon Floor", image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg"}
+        ];
+
+// passport configation
+app.use(require("express-session")({
+    secret: "you are the best!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+// requring routes
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments",commentRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(req, res){
+    console.log("The YelpCamp started!!")
+});
---
+++ YelpCamp/v7/routes/comments.js
@@ -0,0 +1,55 @@
+
+var express = require("express");
+var router = express.Router({mergeParams: true});
+var Campground  = require("../models/campground");
+var Comment = require("../models/comment");
+
+// comments/new
+router.get("/new", isLoggedIn, function(req,res){
+    
+    // find camground by id
+ 
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+          res.render("comments/new", {campground: campground});
+        }
+    })
+    
+  
+})
+
+//comentes create
+router.post("/", isLoggedIn,  function(req, res){
+    
+    // lookup campground using id
+ 
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect('/campgrounds');
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if (err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
+        }
+    });
+    
+});
+
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+module.exports = router;
